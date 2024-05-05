da0df4b66c875a399053840c20f9c8942629ef71
phanhisme <phanhhisme@gmail.com>
2024-05-06 01:38:41 +0700

    Cards Creation

--- Assets/Scripts/Card/CardUI.cs
+++ Assets/Scripts/Card/CardUI.cs
@@ -6,13 +6,24 @@ using TMPro;
 
 public class CardUI : MonoBehaviour
 {
-    public Image image;
+    public Image borderType;
+    public Image icon;
+
     public TextMeshProUGUI cardName;
+    public TextMeshProUGUI cardDes;
+    public TextMeshProUGUI cardType;
+    public TextMeshProUGUI cost;
 
 
     public void UpdateUI(CardData card)
     {
-        image.sprite = card.icon;
+        icon.sprite = card.icon;
+        borderType.sprite = card.cardFront;
+
         cardName.text = card.cardName;
+        cardDes.text = card.cardDes;
+
+        cardType.text = card.cardType.ToString();
+        cost.text = card.stamCost.ToString();
     }
 }
